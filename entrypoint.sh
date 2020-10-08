9c576fee68c7dad960989752113580a5c101d239
Ricardo Sanfelice <ricardo.sanfelice@vrsoft.com.br>
2020-10-08 14:41:55 -0300

    Changing entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,34 @@ NEW_TAG="$1"
 LAST_TAG=$(git describe --abbrev=0)
 TEMP_FILE="/tmp/vr"
 
+echo "Current Folder"
+pwd
+ls
+
+echo "Configuring GIT"
+
 sh -c "git config --global user.name '${GITHUB_ACTOR}' \
       && git config --global user.email '${GITHUB_ACTOR}@users.noreply.github.com'"
 
 if [ ! -f "$CHANGELOG" ]; then
+    echo "Creating CHANGELOG.md"
     touch "$CHANGELOG"
     echo -e "# CHANGELOG\n\n" > $CHANGELOG
 fi
 
+
+echo "Getting messages log"
 git log --format="- %s" $LAST_TAG... --no-merges > $TEMP_FILE
 
-sed -i "3s/^/## $NEW_TAG\n\n\n/" CHANGELOG.md
 
+echo "Updating changelog"
+sed -i "3s/^/## $NEW_TAG\n\n\n/" CHANGELOG.md
 sed -i "4r temp" CHANGELOG.md
 
+echo "Generating commit"
 git commit -a -m "Entrega da versão $NEW_TAG"
+
+echo "Creating tag"
 git tag -a $NEW_TAG -m "$(cat $TEMP_FILE)"
 
 git push --follow-tags
