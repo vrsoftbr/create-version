632c042a2540c41209c5c9e282fcfa4da7e42dd4
Ricardo Sanfelice <ricardo.sanfelice@vrsoft.com.br>
2020-10-08 14:51:03 -0300

    Testing entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,37 @@ set -e
 echo "Current Folder"
 pwd
 ls
-git status
-git log
-git tag -l
+git branch 
+
+echo  $GITHUB_JOB
+echo  $GITHUB_REF
+echo  $GITHUB_SHA
+echo  $GITHUB_REPOSITORY
+echo  $GITHUB_REPOSITORY_OWNER
+echo  $GITHUB_RUN_ID
+echo  $GITHUB_RUN_NUMBER
+echo  $GITHUB_RETENTION_DAYS
+echo  $GITHUB_ACTOR
+echo  $GITHUB_WORKFLOW
+echo  $GITHUB_HEAD_REF
+echo  $GITHUB_BASE_REF
+echo  $GITHUB_EVENT_NAME
+echo  $GITHUB_SERVER_URL
+echo  $GITHUB_API_URL
+echo  $GITHUB_GRAPHQL_URL
+echo  $GITHUB_WORKSPACE
+echo  $GITHUB_ACTION
+echo  $GITHUB_EVENT_PATH
+echo  $GITHUB_PATH
+echo  $GITHUB_ENV
+echo  $RUNNER_OS
+echo  $RUNNER_TOOL_CACHE
+echo  $RUNNER_TEMP
+echo  $RUNNER_WORKSPACE
+echo  $ACTIONS_RUNTIME_URL
+echo  $ACTIONS_RUNTIME_TOKEN
+echo  $ACTIONS_CACHE_URL
+
 
 CHANGELOG="CHANGELOG.md"
 NEW_TAG="$1"
