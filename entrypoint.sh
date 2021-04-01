6959c3848aa3fceda861a1dfabb847d385589f76
robsonscatolon <71078802+robsonscatolon@users.noreply.github.com>
2021-04-01 09:40:04 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -73,7 +73,7 @@ git push origin $BRANCH
 
 pip3 install requests
 
-if ! [ -z "$DIRECTORY" ]; then
+if [ "$DIRECTORY" != ''s ]; then
     python3 /create_tag.py -t $NEW_TAG -c $COMMIT - r "vrsoftbr/$DIRECTORY"
 else 
     python3 /create_tag.py -t $NEW_TAG -c $COMMIT - r $GITHUB_REPOSITORY
